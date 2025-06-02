bb6277ebf4c92e6ebd9c3d5999fecb66b952e0ca
slowpiou <loicprt@gmail.com>
2025-06-02 11:23:12 +0200

    add demos view

--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 import OfficialView from '@/views/OfficialView.vue';
 import GoodiesView from '@/views/GoodiesView.vue';
+import DemosView from '@/views/DemosView.vue';
 
 const routes = [
 	{
@@ -12,6 +13,11 @@ const routes = [
 		path: '/goodies',
 		name: 'goodies',
 		component: GoodiesView,
+	},
+	{
+		path: '/demos',
+		name: 'demos',
+		component: DemosView,
 	},
 	// {
 	//   path: '/about',
