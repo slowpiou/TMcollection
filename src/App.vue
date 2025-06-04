d3350c0b7aa731afd9f26e8d18463f11e94c6134
slowpiou <loicprt@gmail.com>
2025-06-04 13:30:16 +0200

    navbar style

--- src/App.vue
+++ src/App.vue
@@ -11,84 +11,109 @@
 			</div>
 
 			<div class="navbar-menu is-justify-content-space-between">
-				<div class="is-flex">
-					<RouterLink to="/" class="navbar-item">Official</RouterLink>
-					<RouterLink to="/goodies" class="navbar-item">Goodies</RouterLink>
-					<RouterLink to="/demos" class="navbar-item">Demos</RouterLink>
-				</div>
-				<div class="content mb-0">
-					<h1 class="pt-3 title is-size-4 mb-0">TM Collection</h1>
-					<p class="is-italic is-size-7 has-text-right">by SUPERCOBY</p>
-				</div>
-				<div class="is-flex is-align-items-center">
-					<div class="dropdown mr-3" :class="showFilterDropdown ? 'is-active' : ''">
-						<div class="dropdown-trigger" @click="showFilterDropdown = !showFilterDropdown">
-							<button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
-								<span>Games Filters</span>
-								<span class="icon is-small">
-									<i class="fas fa-angle-down" aria-hidden="true"></i>
-								</span>
-							</button>
-						</div>
-						<div class="dropdown-menu" id="dropdown-menu" role="menu">
-							<div class="dropdown-content">
-								<a class="dropdown-item" :class="ownFilter === true ? 'has-text-success' : ''" @click="ownFilter = true">Owned</a>
-								<a class="dropdown-item" :class="ownFilter === false ? 'has-text-success' : ''" @click="ownFilter = false"
-									>Not owned</a
-								>
-								<a class="dropdown-item" :class="ownFilter === undefined ? 'has-text-success' : ''" @click="ownFilter = undefined"
-									>All</a
-								>
-								<hr class="dropdown-divider" />
-								<div class="dropdown-item is-size-7 pl-2 pb-1">Rarity</div>
-								<a class="dropdown-item" :class="rarityFilter === 'Gem' ? 'has-text-success' : ''" @click="rarityFilter = 'Gem'"
-									>Gem</a
-								>
-								<a
-									class="dropdown-item"
-									:class="rarityFilter === 'Very Rare' ? 'has-text-success' : ''"
-									@click="rarityFilter = 'Very Rare'"
-									>Very rare</a
-								>
-								<a class="dropdown-item" :class="rarityFilter === 'Rare' ? 'has-text-success' : ''" @click="rarityFilter = 'Rare'"
-									>Rare</a
-								>
-								<a class="dropdown-item" :class="rarityFilter === 'Common' ? 'has-text-success' : ''" @click="rarityFilter = 'Common'"
-									>Common</a
-								>
-								<a class="dropdown-item" :class="rarityFilter === 'All' ? 'has-text-success' : ''" @click="rarityFilter = 'All'"
-									>All</a
-								>
+				<div class="container is-fluid">
+					<div class="columns">
+						<div class="column is-5">
+							<div class="is-flex">
+								<RouterLink to="/" class="navbar-item">Official</RouterLink>
+								<RouterLink to="/goodies" class="navbar-item">Goodies</RouterLink>
+								<RouterLink to="/demos" class="navbar-item">Demos</RouterLink>
 							</div>
 						</div>
-					</div>
-					<div class="control has-icons-left mr-3" v-if="$route.name === 'home'">
-						<input class="input" type="text" placeholder="Search game" v-model="searchGame" id="search-game" />
-						<span class="icon is-small is-left">
-							<i class="fas fa-search"></i>
-						</span>
-					</div>
-					<div id="socials" class="buttons is-justify-content-center">
-						<a href="https://discord.gg/QhfB3UKSe5" class="button socials-button" target="_blank">
-							<span class="icon is-small">
-								<i class="fab fa-discord"></i>
-							</span>
-						</a>
-						<a href="https://x.com/cobrasktm" class="button socials-button" target="_blank">
-							<span class="icon is-small">
-								<i class="fab fa-x-twitter"></i>
-							</span>
-						</a>
-						<a href="https://www.youtube.com/@cobytm" class="button socials-button" target="_blank">
-							<span class="icon is-small">
-								<i class="fab fa-youtube"></i>
-							</span>
-						</a>
-						<a href="https://github.com/SuperCoby/" class="button socials-button" target="_blank">
-							<span class="icon is-small">
-								<i class="fab fa-github"></i>
-							</span>
-						</a>
+						<div class="column is-2">
+							<div class="content has-text-centered mb-0">
+								<h1 class="pt-3 title is-size-4 mb-0">TM Collection</h1>
+								<p class="is-italic is-size-7">by SUPERCOBY</p>
+							</div>
+						</div>
+						<div class="column is-5 is-flex is-align-items-center is-justify-content-flex-end">
+							<div class="control has-icons-left mr-3" v-if="$route.name === 'home'">
+								<input class="input" type="text" placeholder="Search game" v-model="searchGame" id="search-game" />
+								<span class="icon is-small is-left">
+									<i class="fas fa-search"></i>
+								</span>
+							</div>
+							<div class="dropdown mr-3" :class="showFilterDropdown ? 'is-active' : ''" v-if="$route.name === 'home'">
+								<div class="dropdown-trigger" @click="showFilterDropdown = !showFilterDropdown">
+									<button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+										<span>Games Filters</span>
+										<span class="icon is-small">
+											<i class="fas fa-angle-down" aria-hidden="true"></i>
+										</span>
+									</button>
+								</div>
+								<div class="dropdown-menu" id="dropdown-menu" role="menu">
+									<div class="dropdown-content">
+										<a class="dropdown-item" :class="ownFilter === true ? 'has-text-success' : ''" @click="ownFilter = true"
+											>Owned</a
+										>
+										<a class="dropdown-item" :class="ownFilter === false ? 'has-text-success' : ''" @click="ownFilter = false"
+											>Not owned</a
+										>
+										<a
+											class="dropdown-item"
+											:class="ownFilter === undefined ? 'has-text-success' : ''"
+											@click="ownFilter = undefined"
+											>All</a
+										>
+										<hr class="dropdown-divider" />
+										<div class="dropdown-item is-size-7 pl-2 pb-1">Rarity</div>
+										<a
+											class="dropdown-item"
+											:class="rarityFilter === 'Gem' ? 'has-text-success' : ''"
+											@click="rarityFilter = 'Gem'"
+											>Gem</a
+										>
+										<a
+											class="dropdown-item"
+											:class="rarityFilter === 'Very Rare' ? 'has-text-success' : ''"
+											@click="rarityFilter = 'Very Rare'"
+											>Very rare</a
+										>
+										<a
+											class="dropdown-item"
+											:class="rarityFilter === 'Rare' ? 'has-text-success' : ''"
+											@click="rarityFilter = 'Rare'"
+											>Rare</a
+										>
+										<a
+											class="dropdown-item"
+											:class="rarityFilter === 'Common' ? 'has-text-success' : ''"
+											@click="rarityFilter = 'Common'"
+											>Common</a
+										>
+										<a
+											class="dropdown-item"
+											:class="rarityFilter === 'All' ? 'has-text-success' : ''"
+											@click="rarityFilter = 'All'"
+											>All</a
+										>
+									</div>
+								</div>
+							</div>
+							<div id="socials" class="buttons is-justify-content-center">
+								<a href="https://discord.gg/QhfB3UKSe5" class="button socials-button" target="_blank">
+									<span class="icon is-small">
+										<i class="fab fa-discord"></i>
+									</span>
+								</a>
+								<a href="https://x.com/cobrasktm" class="button socials-button" target="_blank">
+									<span class="icon is-small">
+										<i class="fab fa-x-twitter"></i>
+									</span>
+								</a>
+								<a href="https://www.youtube.com/@cobytm" class="button socials-button" target="_blank">
+									<span class="icon is-small">
+										<i class="fab fa-youtube"></i>
+									</span>
+								</a>
+								<a href="https://github.com/SuperCoby/" class="button socials-button" target="_blank">
+									<span class="icon is-small">
+										<i class="fab fa-github"></i>
+									</span>
+								</a>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -142,6 +167,9 @@ nav a {
 .input:focus {
 	border-color: white;
 	box-shadow: rgba(255, 255, 255, 0.25) 0px 0px 0px 3px;
+}
+#search-game {
+	max-width: 200px;
 }
 
 @import 'vue3-carousel/carousel.css';
