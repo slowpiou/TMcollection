8861c17db52655507d9b64e5599eb65bc29718ce
SuperCoby <cobrask@hotmail.fr>
2025-06-05 22:45:40 +0200

    Add Picture / Goodies.js

--- src/goodies.js
+++ src/goodies.js
@@ -1,66 +1,192 @@
 const GOODIES = [
 	{
-		title: 'Basic Example',
+		title: 'tshirt tmnf',
 		image: {
-			src: ['https://picsum.photos/400/300?random=1', 'https://picsum.photos/400/300?random=2'],
-			alt: 'PC Original Big Box BigBox own Commun 0646662101145 USA',
+			src: [
+				require('@/assets/goodies/TM1-2/4e3add5c-2bb2-45dc-b6e8-a0de975c9099.webp'),
+				require('@/assets/goodies/TM1-2/4e3add5c-2bb2-45dc-b6e8-a0de975c9099_1.webp')
+			],
+			alt: 'tshirt tmnf',
 		},
 		type: 'TM1-2',
 		own: true,
 	},
 	{
-		title: 'Basic Example',
+		title: 'red hat',
 		image: {
-			src: ['https://picsum.photos/400/300?random=1', 'https://picsum.photos/400/300?random=2'],
-			alt: 'PC Original Big Box BigBox own Commun 0646662101145 USA',
+			src: [
+				require('@/assets/goodies/TM1-2/00d40845-981d-4509-b378-e1a28e307194.webp'),
+			],
+			alt: 'red hat',
 		},
 		type: 'TM1-2',
 		own: true,
 	},
 	{
-		title: 'Basic Example',
+		title: 'bracelet',
 		image: {
-			src: ['https://picsum.photos/400/300?random=1', 'https://picsum.photos/400/300?random=2'],
-			alt: 'PC Original Big Box BigBox own Commun 0646662101145 USA',
+			src: [
+				require('@/assets/goodies/TM1-2/4aa413f9-f3d0-4d6a-82cf-8f1aeb00bf5e.webp'),
+			],
+			alt: 'bracelet',
 		},
 		type: 'TM1-2',
 		own: true,
 	},
 	{
-		title: 'Basic Example',
+		title: 'Card game',
 		image: {
-			src: ['https://picsum.photos/400/300?random=1', 'https://picsum.photos/400/300?random=2'],
-			alt: 'PC Original Big Box BigBox own Commun 0646662101145 USA',
+			src: [
+				require('@/assets/goodies/TM1-2/5b4955ca-c847-4bc6-a02c-f278236c12a9.webp'),
+				require('@/assets/goodies/TM1-2/5b4955ca-c847-4bc6-a02c-f278236c12a9_1.webp')
+			],
+			alt: 'Card game',
 		},
 		type: 'TM1-2',
 		own: true,
 	},
 	{
-		title: 'Basic Example',
+		title: 'poignet',
 		image: {
-			src: ['https://picsum.photos/400/300?random=1', 'https://picsum.photos/400/300?random=2'],
-			alt: 'PC Original Big Box BigBox own Commun 0646662101145 USA',
+			src: [
+				require('@/assets/goodies/TM1-2/5e8f9113-ab2b-4c44-be39-32bb9515ba77.webp'),
+			],
+			alt: 'poignet',
+		},
+		type: 'TM1-2',
+		own: false,
+	},
+	{
+		title: 'Mousepad Maniaplanet',
+		image: {
+			src: [
+				require('@/assets/goodies/TM1-2/06a0654b-d726-44de-8489-2af111298e09.webp'),
+			],
+			alt: 'Mousepad Maniaplanet',
 		},
 		type: 'TM1-2',
 		own: true,
 	},
 	{
-		title: 'Basic Example',
+		title: 'Poster TMUF',
 		image: {
-			src: ['https://picsum.photos/400/300?random=1', 'https://picsum.photos/400/300?random=2'],
-			alt: 'PC Original Big Box BigBox own Commun 0646662101145 USA',
+			src: [
+				require('@/assets/goodies/TM1-2/7cf4a548-0af5-430b-a7e1-c850202a6909.webp'),
+			],
+			alt: 'Poster TMUF',
+		},
+		type: 'TM1-2',
+		own: false,
+	},
+	{
+		title: 'Mousepad Stadium2',
+		image: {
+			src: [
+				require('@/assets/goodies/TM1-2/9b7ee3e4-ebd2-45ec-bf78-0a671a69882a.webp'),
+			],
+			alt: 'Mousepad Stadium2',
+		},
+		type: 'TM1-2',
+		own: false,
+	},
+	{
+		title: 'green hat',
+		image: {
+			src: [
+				require('@/assets/goodies/TM1-2/16bdc223-296b-4b6b-b743-2291406dae62.webp'),
+			],
+			alt: 'green hat',
 		},
 		type: 'TM1-2',
 		own: true,
 	},
 	{
-		title: 'Basic Example',
+		title: 'bottle 10 years',
 		image: {
-			src: ['https://picsum.photos/400/300?random=1', 'https://picsum.photos/400/300?random=2'],
-			alt: 'PC Original Big Box BigBox own Commun 0646662101145 USA',
+			src: [
+				require('@/assets/goodies/TM1-2/26a72d9b-77b2-4679-bc09-4127bffa69c2.webp'),
+			],
+			alt: 'bottle 10 years',
 		},
 		type: 'TM1-2',
 		own: true,
+	},
+	{
+		title: 'pins legalize',
+		image: {
+			src: [
+				require('@/assets/goodies/TM1-2/29f05145-4884-4adc-b140-251ae8396dd7.webp'),
+			],
+			alt: 'pins legalize',
+		},
+		type: 'TM1-2',
+		own: true,
+	},
+	{
+		title: 'Canyon circle',
+		image: {
+			src: [
+				require('@/assets/goodies/TM1-2/35ee86b7-19a1-45ef-bf7e-19cacbfcc817.webp'),
+			],
+			alt: 'Canyon circle',
+		},
+		type: 'TM1-2',
+		own: true,
+	},
+	{
+		title: 'Wii wheel',
+		image: {
+			src: [
+				require('@/assets/goodies/TM1-2/56f19ea3-07c8-46c9-9b72-704048d1f5a4.webp'),
+			],
+			alt: 'Wii wheel',
+		},
+		type: 'TM1-2',
+		own: true,
+	},
+	{
+		title: 'Resavation Wii',
+		image: {
+			src: [
+				require('@/assets/goodies/TM1-2/59c6f386-651b-442a-a02c-aba1acb39404.webp'),
+			],
+			alt: 'Resavation Wii',
+		},
+		type: 'TM1-2',
+		own: true,
+	},
+	{
+		title: 'Bracelet Trackmania',
+		image: {
+			src: [
+				require('@/assets/goodies/TM1-2/78e8bb90-623c-4a85-84f3-fe5da68efe6a.webp'),
+			],
+			alt: 'Bracelet Trackmania',
+		},
+		type: 'TM1-2',
+		own: true,
+	},
+	{
+		title: '10 years circle',
+		image: {
+			src: [
+				require('@/assets/goodies/TM1-2/185cfd65-ae1b-49fe-8c6a-4487e640c329.webp'),
+			],
+			alt: '10 years circle',
+		},
+		type: 'TM1-2',
+		own: true,
+	},
+	{
+		title: '15 years',
+		image: {
+			src: [
+				require('@/assets/goodies/TM1-2/450c3b0f-ab50-4716-8cbe-3df1826a9b00.webp'),
+			],
+			alt: '15 years',
+		},
+		type: 'TM1-2',
+		own: false,
 	},
 ];
 
