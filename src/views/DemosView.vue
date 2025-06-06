c9acf6c6835ff9a0bfca5752ff67821de481422b
slowpiou <loicprt@gmail.com>
2025-06-06 06:39:11 +0200

    update demos view

--- src/views/DemosView.vue
+++ src/views/DemosView.vue
@@ -1,5 +1,5 @@
 <template>
-	<item-filter :items="countries" @filteredByItem="filterGamesByCountry"></item-filter>
+	<item-filter :items="countries" @filteredByItem="filterDemosByCountry"></item-filter>
 
 	<div class="container mt-5">
 		<div class="columns is-multiline">
@@ -26,7 +26,7 @@
 		</div>
 		<empty-hero v-if="demosPaginated.length === 0"></empty-hero>
 
-		<!-- <game-modal :game="gameModalContent" v-if="showGameModal" @close-modal="showGameModal = false"></game-modal> -->
+		<game-modal :game="gameModalContent" v-if="showGameModal" @close-modal="showGameModal = false"></game-modal>
 	</div>
 </template>
 
@@ -34,13 +34,14 @@
 import COUNTRIES from '@/countries.js';
 import DEMOS from '@/demos.js';
 import ItemFilter from '@/components/ItemFilter.vue';
+import GameModal from '@/components/GameModal.vue';
 import OwnedOrNot from '@/components/OwnedOrNot.vue';
 import EmptyHero from '@/components/EmptyHero.vue';
 import { Carousel, Slide, Navigation } from 'vue3-carousel';
 export default {
 	name: 'DemosView',
-	props: ['modelValue'],
-	components: { Carousel, Slide, Navigation, ItemFilter, EmptyHero, OwnedOrNot },
+	props: ['ownFilter'],
+	components: { Carousel, Slide, Navigation, GameModal, ItemFilter, EmptyHero, OwnedOrNot },
 	data() {
 		return {
 			demos: DEMOS,
@@ -50,21 +51,38 @@ export default {
 			filteredDemos: DEMOS,
 			itemsToShow: 1,
 			selectedCountry: '',
-			// showGameModal: false,
-			// gameModalContent: null,
+			showGameModal: false,
+			gameModalContent: null,
 			currentPage: 1,
-			perPage: 16,
+			perPage: 18,
 		};
 	},
 	computed: {
 		demosPaginated() {
-			return this.filteredDemos.slice(0, this.currentPage * this.perPage);
+			let ownFilteredDemos = this.ownFilter !== undefined ? this.filteredDemos.filter((g) => g.own === this.ownFilter) : this.filteredDemos;
+
+			return ownFilteredDemos.slice(0, this.currentPage * this.perPage);
 		},
 	},
 	methods: {
-		filterGamesByCountry(c) {
+		filterDemosByCountry(c) {
 			this.filteredDemos = c !== '' ? this.demos.filter((g) => g.country.includes(c)) : this.demos;
 		},
+		openGameModal(g) {
+			this.gameModalContent = g;
+			this.showGameModal = true;
+		},
+		handleScroll() {
+			if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 100) {
+				this.currentPage++;
+			}
+		},
+	},
+	created() {
+		window.addEventListener('scroll', this.handleScroll);
+	},
+	unmounted() {
+		window.removeEventListener('scroll', this.handleScroll);
 	},
 };
 </script>
