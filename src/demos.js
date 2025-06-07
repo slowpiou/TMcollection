bddfc754283c8eb92eb619a5032a6af53986d500
SuperCoby <cobrask@hotmail.fr>
2025-06-07 18:10:17 +0200

    add Demos

--- src/demos.js
+++ src/demos.js
@@ -1,66 +1,695 @@
 const DEMOS = [
 	{
-		title: 'Basic Example',
+		title: 'Missing',
 		image: {
-			src: ['https://picsum.photos/400/300?random=1', 'https://picsum.photos/400/300?random=2'],
-			alt: 'PC Original Big Box BigBox own Commun 0646662101145 USA',
-		},
-		country: 'USA',
-		own: true,
+			src: [
+				require('@/assets/demos/brazil/971a0999-776d-42c3-a044-0fa295b26aea.webp'),
+			],
+			country: 'brazil',
+			own: false,
+		}
 	},
 	{
-		title: 'Basic Example',
+		title: 'Missing',
 		image: {
-			src: ['https://picsum.photos/400/300?random=1', 'https://picsum.photos/400/300?random=2'],
-			alt: 'PC Original Big Box BigBox own Commun 0646662101145 USA',
-		},
-		country: 'USA',
-		own: true,
+			src: [
+				require('@/assets/demos/czech/8d9b316e-cdc2-420d-85c5-9053197b568f.webp'),
+			],
+			country: 'czech',
+			own: false,
+		}
 	},
 	{
-		title: 'Basic Example',
+		title: 'Missing',
 		image: {
-			src: ['https://picsum.photos/400/300?random=1', 'https://picsum.photos/400/300?random=2'],
-			alt: 'PC Original Big Box BigBox own Commun 0646662101145 USA',
-		},
-		country: 'USA',
-		own: true,
+			src: [
+				require('@/assets/demos/czech/43a2c0cd-8810-493a-baf9-c72a3383f97f.webp'),
+			],
+			country: 'czech',
+			own: false,
+		}
 	},
 	{
-		title: 'Basic Example',
+		title: 'Missing',
 		image: {
-			src: ['https://picsum.photos/400/300?random=1', 'https://picsum.photos/400/300?random=2'],
-			alt: 'PC Original Big Box BigBox own Commun 0646662101145 USA',
-		},
-		country: 'USA',
-		own: true,
+			src: [
+				require('@/assets/demos/czech/38cd79f7-ff7b-4b3b-bb34-71e0f2bea790.webp'),
+			],
+			country: 'czech',
+			own: false,
+		}
 	},
 	{
-		title: 'Basic Example',
+		title: 'Missing',
 		image: {
-			src: ['https://picsum.photos/400/300?random=1', 'https://picsum.photos/400/300?random=2'],
-			alt: 'PC Original Big Box BigBox own Commun 0646662101145 USA',
-		},
-		country: 'USA',
-		own: true,
+			src: [
+				require('@/assets/demos/czech/93563b1c-d3dc-4b67-874b-74f1033f7141.webp'),
+			],
+			country: 'czech',
+			own: false,
+		}
 	},
 	{
-		title: 'Basic Example',
+		title: 'Missing',
 		image: {
-			src: ['https://picsum.photos/400/300?random=1', 'https://picsum.photos/400/300?random=2'],
-			alt: 'PC Original Big Box BigBox own Commun 0646662101145 USA',
-		},
-		country: 'USA',
-		own: true,
+			src: [
+				require('@/assets/demos/czech/f9cff9a1-004f-4951-81f5-ef1b1b979da1.webp'),
+			],
+			country: 'czech',
+			own: false,
+		}
 	},
 	{
-		title: 'Basic Example',
+		title: 'Missing',
 		image: {
-			src: ['https://picsum.photos/400/300?random=1', 'https://picsum.photos/400/300?random=2'],
-			alt: 'PC Original Big Box BigBox own Commun 0646662101145 USA',
-		},
-		country: 'USA',
-		own: true,
+			src: [
+				require('@/assets/demos/czech/a513f58a-5450-4751-aa20-7e50b791a617.webp'),
+			],
+			country: 'czech',
+			own: false,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/france/0d5c66c7-5abb-480f-90c1-0f5897015d7a.webp'),
+				require('@/assets/demos/france/0d5c66c7-5abb-480f-90c1-0f5897015d7a_1.webp'),
+			],
+			country: 'france',
+			own: true,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/france/1a116513-0d9c-4d14-8d5f-be614b62b8a9.webp'),
+			],
+			country: 'france',
+			own: false,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/france/4e7dc0d1-2f21-4b12-a9d5-1c30c49c8acb.webp'),
+				require('@/assets/demos/france/4e7dc0d1-2f21-4b12-a9d5-1c30c49c8acb_1.webp'),
+			],
+			country: 'france',
+			own: true,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/france/5de16842-3042-44ff-8c53-0e076237fd4f.webp'),
+			],
+			country: 'france',
+			own: true,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/france/8a1386e4-4036-4ca7-9679-867f2af82952.webp'),
+			],
+			country: 'france',
+			own: true,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/france/8aa8c645-5a16-44c1-a6f3-7d2a60d4c048.webp'),
+			],
+			country: 'france',
+			own: true,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/france/8e2b0940-138e-470f-9622-403694ada850.webp'),
+			],
+			country: 'france',
+			own: true,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/france/12fa8c38-dc7a-4ea1-9002-b22ba4702ae5.webp'),
+			],
+			country: 'france',
+			own: true,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/france/39ee9c71-5541-43d4-9c21-9d7ee0ff69e7.webp'),
+			],
+			country: 'france',
+			own: true,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/france/52a8c162-e51e-4d67-9f99-94a92473a36c.webp'),
+			],
+			country: 'france',
+			own: false,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/france/61c91284-d260-46b5-82b2-cad86d33c4e2.webp'),
+				require('@/assets/demos/france/61c91284-d260-46b5-82b2-cad86d33c4e2_1.webp'),
+			],
+			country: 'france',
+			own: true,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/france/64e5bbc0-4ca3-43dd-80ef-9e2867c595df.webp'),
+			],
+			country: 'france',
+			own: false,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/france/34662f72-7353-49de-8178-c0cce6d60944.webp'),
+			],
+			country: 'france',
+			own: true,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/france/9538364b-fae8-4d12-bbad-71905a026c9a.webp'),
+			],
+			country: 'france',
+			own: true,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/france/a6c1d5b8-e9ec-4756-96d2-1aacdaf7f7e8.webp'),
+				require('@/assets/demos/france/a6c1d5b8-e9ec-4756-96d2-1aacdaf7f7e8_1.webp'),
+			],
+			country: 'france',
+			own: false,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/france/dbc4be0f-8623-4d4a-8135-f5e784fdcdcb.webp'),
+			],
+			country: 'france',
+			own: true,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/france/e788c001-409d-40d5-8c64-0956b5dee31b.webp'),
+			],
+			country: 'france',
+			own: true,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/france/f8c58e2f-d0a2-45d5-9b01-b1101850012d.webp'),
+				require('@/assets/demos/france/f8c58e2f-d0a2-45d5-9b01-b1101850012d_1.webp'),
+			],
+			country: 'france',
+			own: true,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/germany/24dcd0cb-a3a4-443b-80d1-7f8f3f907fc1.webp'),
+			],
+			country: 'germany',
+			own: true,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/germany/ae2220bf-cca3-4883-bfde-23efbc7910ef.webp'),
+				require('@/assets/demos/germany/ae2220bf-cca3-4883-bfde-23efbc7910ef_1.webp'),
+			],
+			country: 'germany',
+			own: true,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/germany/be615d99-0331-4d8f-a2e6-6443783b5acf.webp'),
+			],
+			country: 'germany',
+			own: false,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/germany/ec4e369b-8d97-4d79-83d1-e83113d91c00.webp'),
+			],
+			country: 'germany',
+			own: false,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/germany/ef2700d6-7d22-4891-b18b-c72638c42701.webp'),
+			],
+			country: 'germany',
+			own: false,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/germany/fe14e27b-1882-43c9-b728-d99304cc70c1.webp'),
+			],
+			country: 'germany',
+			own: false,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/italy/02a74b09-f63c-4b8a-8fe7-901b2c68e118.webp'),
+			],
+			country: 'italy',
+			own: false,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/italy/3aed9605-5c2c-4523-8235-169a45166d2d.webp'),
+			],
+			country: 'italy',
+			own: false,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/italy/4b95c7ee-ad15-4a95-a862-3c558b119014.webp'),
+			],
+			country: 'italy',
+			own: false,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/italy/85fd84f4-1433-4470-a62e-6e7fc2206a56.webp'),
+				require('@/assets/demos/italy/85fd84f4-1433-4470-a62e-6e7fc2206a56_1.webp'),
+			],
+			country: 'italy',
+			own: false,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/italy/b0447351-9474-41e7-9430-135101aada9c.webp'),
+				require('@/assets/demos/italy/b0447351-9474-41e7-9430-135101aada9c_1.webp'),
+			],
+			country: 'italy',
+			own: true,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/italy/ced45ebe-9f61-479e-8c1e-e4af7d4792a3.webp'),
+			],
+			country: 'italy',
+			own: true,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/italy/fda16e76-5a32-4210-9473-bfd596202d27.webp'),
+			],
+			country: 'italy',
+			own: true,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/poland/3e74d0d4-2195-4a45-b2f9-4bfbe4f640a5.webp'),
+			],
+			country: 'poland',
+			own: false,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/poland/5d2f15ae-b297-4755-9e17-a875dca770c4.webp'),
+				require('@/assets/demos/poland/5d2f15ae-b297-4755-9e17-a875dca770c4_1.webp'),
+			],
+			country: 'poland',
+			own: true,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/poland/30e0b58c-0ad0-49ef-ba1a-074b2f107658.webp'),
+			],
+			country: 'poland',
+			own: true,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/poland/428dff1b-277b-49c2-81b1-773bd1e647aa.webp'),
+			],
+			country: 'poland',
+			own: false,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/poland/4000edad-b081-4bf1-8c02-15e9d2c16534.webp'),
+			],
+			country: 'poland',
+			own: true,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/poland/cab2412a-70df-45d7-8df9-a7b5d73c97c0.webp'),
+				require('@/assets/demos/poland/cab2412a-70df-45d7-8df9-a7b5d73c97c0_1.webp'),
+			],
+			country: 'poland',
+			own: true,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/portugal/3d6bc333-32ac-49c4-9591-d707d4c9e192.webp'),
+				require('@/assets/demos/portugal/3d6bc333-32ac-49c4-9591-d707d4c9e192_1.webp'),
+			],
+			country: 'portugal',
+			own: true,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/portugal/4d8e2b22-27b3-4842-a950-22c0ba668eeb.webp'),
+				require('@/assets/demos/portugal/4d8e2b22-27b3-4842-a950-22c0ba668eeb_1.webp'),
+			],
+			country: 'portugal',
+			own: false,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/portugal/4edb4a07-18bf-49df-8d49-27cbf13c0cfb.webp'),
+				require('@/assets/demos/portugal/4edb4a07-18bf-49df-8d49-27cbf13c0cfb_1.webp'),
+			],
+			country: 'portugal',
+			own: true,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/portugal/5aa8f507-e64e-434a-ba85-4667e0fb0bc4.webp'),
+				require('@/assets/demos/portugal/5aa8f507-e64e-434a-ba85-4667e0fb0bc4_1.webp'),
+			],
+			country: 'portugal',
+			own: true,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/portugal/5ed8884d-3299-40cc-9ace-8247d7ea3bef.webp'),
+			],
+			country: 'portugal',
+			own: false,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/portugal/477fe162-a74c-4d1c-97a5-a5446e292567.webp'),
+			],
+			country: 'portugal',
+			own: false,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/portugal/2829dfcf-5e73-4733-ad03-13e6ca43580b.webp'),
+				require('@/assets/demos/portugal/2829dfcf-5e73-4733-ad03-13e6ca43580b_1.webp'),
+			],
+			country: 'portugal',
+			own: false,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/portugal/cad06e00-0933-4385-a8db-9d8a1e4449bb.webp'),
+				require('@/assets/demos/portugal/cad06e00-0933-4385-a8db-9d8a1e4449bb_1.webp'),
+			],
+			country: 'portugal',
+			own: false,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/russia/1c378f42-4e2d-4bcb-a983-c959af3b448f.webp'),
+				require('@/assets/demos/russia/1c378f42-4e2d-4bcb-a983-c959af3b448f_1.webp'),
+			],
+			country: 'russia',
+			own: true,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/russia/1ebe2d05-292b-4b37-bbc8-9efd7899be2c.webp'),
+				require('@/assets/demos/russia/1ebe2d05-292b-4b37-bbc8-9efd7899be2c_1.webp'),
+			],
+			country: 'russia',
+			own: true,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/russia/5e4f8c50-5428-4619-ba09-faffb80068be.webp'),
+			],
+			country: 'russia',
+			own: false,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/russia/6c668fd0-b255-4578-a201-02675a149873.webp'),
+			],
+			country: 'russia',
+			own: false,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/russia/84f275bb-3516-42f9-aad6-6fb9abb4e944.webp'),
+				require('@/assets/demos/russia/84f275bb-3516-42f9-aad6-6fb9abb4e944_1.webp'),
+			],
+			country: 'russia',
+			own: true,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/russia/7060c73e-70ee-47b7-a14c-c29edb36cf37.webp'),
+				require('@/assets/demos/russia/7060c73e-70ee-47b7-a14c-c29edb36cf37_1.webp'),
+			],
+			country: 'russia',
+			own: true,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/russia/86077ab6-c0ad-48bc-b6d8-9c330d42666a.webp'),
+				require('@/assets/demos/russia/86077ab6-c0ad-48bc-b6d8-9c330d42666a_1.webp'),
+			],
+			country: 'russia',
+			own: true,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/russia/a49930ad-6809-4eb3-be24-de182dee3c56.webp'),
+			],
+			country: 'russia',
+			own: false,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/russia/ad25eb0a-3cfb-41a7-8b59-263015e4b009.webp'),
+			],
+			country: 'russia',
+			own: false,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/russia/ddd1aa75-a03e-41f1-87a0-814def8590db.webp'),
+			],
+			country: 'russia',
+			own: false,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/russia/e16f2dd7-7eee-4ae2-9264-114d8fa2954e.webp'),
+				require('@/assets/demos/russia/e16f2dd7-7eee-4ae2-9264-114d8fa2954e_1.webp'),
+			],
+			country: 'russia',
+			own: true,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/uk/2f13f577-49ac-453c-ae65-b31aa73656f9.webp'),
+			],
+			country: 'uk',
+			own: false,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/uk/b9702b68-bba0-4495-af49-487f85ee20fb.webp'),
+			],
+			country: 'uk',
+			own: false,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/india/a2c8eccb-d300-44ba-96df-70284ac8bb6f.webp'),
+			],
+			country: 'india',
+			own: false,
+		}
+	},
+	{
+		title: 'Missing',
+		image: {
+			src: [
+				require('@/assets/demos/india/fc0a0eb2-33bd-451a-bd58-bbe99b87aac4.webp'),
+			],
+			country: 'india',
+			own: false,
+		}
 	},
 ];
 
